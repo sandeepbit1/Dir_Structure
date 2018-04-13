90ff4548f6af699bd42dbdbb4b9511280bba27e8
Sandeep kumar <sandeepbit87@gmail.com>
2018-04-13 16:54:43 +0530

    Second Commit

--- app/controllers/file_strs_controller.rb
+++ app/controllers/file_strs_controller.rb
@@ -14,15 +14,33 @@ class FileStrsController < ApplicationController
   def edit
     @file_str = FileStr.find(params[:id])
   end
+
   def create
     @dir_str = DirStr.find(params[:dir_str_id])
     @file_str = @dir_str.file_strs.create(file_str_params)
     redirect_to dir_str_path(@dir_str)
   end
 
+  def update
+    @_strs = FileStr.find(params[:id])
+    if @file_strs.update(file_str_params)
+      redirect_to @file_strs
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @dir_str = DirStr.find(params[:dir_str_id])
+    @file_str = @dir_str.file_strs.find(params[:id])
+    @file_str.destroy
+    redirect_to dir_str_path(@dir_str)
+  end
+
+
   private
   def file_str_params
-    params.require(:comment).permit(:name, :parent_dir, :owner)
+    params.require(:file_str).permit(:name, :parent_dir, :owner)
   end
 
 end
